447c75cab06170e778caa086be0a1e02d6e42204
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-01-05 14:57:10 +0530

    'pushing-to-do-list-app'

--- TIC TAC TOE game/app.js
+++ TIC TAC TOE game/app.js
@@ -3,6 +3,7 @@ let resetBtn = document.querySelector("#reset-btn");
 let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
+
 let turnO = true;
 
 const winPatterns = [
